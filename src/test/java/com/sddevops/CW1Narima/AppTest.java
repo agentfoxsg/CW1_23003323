4dc1ee71ef3117515f6bbcb696bfdcda154e0505
agentfoxsg <narima.nasir@gmail.com>
2023-07-14 11:39:15 -0700

    Initial Commit

---
+++ src/test/java/com/sddevops/CW1Narima/AppTest.java
@@ -0,0 +1,20 @@
+package com.sddevops.CW1Narima;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
